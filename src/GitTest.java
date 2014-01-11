6c978c422a676ed13206f0500e20153e8f139bd7
yiqing.xiao <xiaoyiqing90@gmail.com>
2014-01-11 16:20:37 -0500

    commit number 3

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,8 +1,13 @@
 
 public class GitTest {
+	
+	private int i;
+	
 	public static void main(String[]args){
 		System.out.println("Hi There!");
 		
 		System.out.println("Welcome to the jungle");
+		
+		
 	}
 }
